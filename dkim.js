2110b9bae314099c97c2a59b3491b6395032991f
jhermsmeier <jhermsmeier@googlemail.com>
2012-12-22 17:16:50 +0100

    Initial commit

---
+++ dkim.js
@@ -0,0 +1,42 @@
+
+function DKIM( signature ) {
+  
+  if( !(this instanceof DKIM) )
+    return new DKIM( signature )
+  
+  this.parse( signature )
+  
+}
+
+DKIM.fields = {
+  v: 'version',
+  a: 'signing_algorithm',
+  c: 'canonicalization',
+  q: 'query_method',
+  l: 'length',
+  t: 'timestamp',
+  x: 'expires',
+  h: 'headers',
+  bh: 'body_hash',
+  s: 'selector',
+  d: 'signing_domain',
+  b: 'signature'
+}
+
+DKIM.prototype = {
+  
+  parse: function( signature ) {
+    
+    var field, fields = signature.split( '; ' )
+    
+    for( var i in fields ) {
+      if( field = fields[i].match( /^([a-z]+?)=(.+?)$/i ) ) {
+        this[ DKIM.fields[ field[1] ] ] = field[2]
+      }
+    }
+    
+  }
+  
+}
+
+module.exports = DKIM
