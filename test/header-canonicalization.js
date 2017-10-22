5bff20b387f6565a273c85a9810cdf35b347adf6
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-10-22 18:43:11 +0545

    add a test case for unordered headers

--- test/header-canonicalization.js
+++ test/header-canonicalization.js
@@ -35,7 +35,23 @@ describe( 'DKIM', function() {
         assert.equal( result, 'from:X\r\nto:Y\r\nsubject:Y Z\r\ndate:Sun, 22 Oct 2017 10:43:49 +0000\r\ndkim-signature:v=1; a=rsa-sha256; d=example.net; s=brisbane; c=simple; q=dns/txt; i=@eng.example.net; t=1117574938; x=1118006938; h=from:to:subject:date; z=From:foo@eng.example.net|To:joe@example.com|Subject:demo=20run|Date:July=205,=202005=203:44:08=20PM=20-0700; bh=MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI=; b=' )
       })
 
-      it('Preserves the order of signing headers')
+      it('Preserves the order of signing headers', function() {
+        var headers = [
+          'From: X',
+          'To : Y  ',
+          'Date : Sun, 22 Oct 2017 10:43:49 +0000  ',
+          'Subject : Y\t\r\n\tZ  ',
+          `DKIM-Signature: v=1; a=rsa-sha256; d=example.net; s=brisbane; c=simple; q=dns/txt; i=@eng.example.net; t=1117574938; x=1118006938; h=from:to:subject:date; z=From:foo@eng.example.net|To:joe@example.com|Subject:demo=20run|Date:July=205,=202005=203:44:08=20PM=20-0700; bh=MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI=; b=dzdVyOfAKCdLXdJOc9G2q8LoXSlEniSbav+yuU4zGeeruD00lszZVoG4ZHRNiYzR`
+        ]
+        var sigHeaders = [
+          'from',
+          'to',
+          'subject',
+          'date',
+        ]
+        var result = DKIM.processHeader( headers, sigHeaders, 'relaxed' )
+        assert.equal( result, 'from:X\r\nto:Y\r\nsubject:Y Z\r\ndate:Sun, 22 Oct 2017 10:43:49 +0000\r\ndkim-signature:v=1; a=rsa-sha256; d=example.net; s=brisbane; c=simple; q=dns/txt; i=@eng.example.net; t=1117574938; x=1118006938; h=from:to:subject:date; z=From:foo@eng.example.net|To:joe@example.com|Subject:demo=20run|Date:July=205,=202005=203:44:08=20PM=20-0700; bh=MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI=; b=' )
+      })
 
     })
 
