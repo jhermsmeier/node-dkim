1c3933444cd3e9175cc4d883c859729e0af3d45c
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-10-22 21:39:19 +0545

    cleanup signing headers * For msg with multiple signatures, create a list of headers that include one and only one signature header * If signature is X-Google-DKIM-Signature then ignore the X-Google- part * Add a new attribute in the result "verified" which is set to true for valid signature and false otherwise * User should check the value of "code" if verified is false, otherwise this "code" value has no mening

---
+++ test/verify.js
@@ -0,0 +1,47 @@
+var assert = require('assert')
+var raw = require('./data/raw-message')
+var DKIM = require('..')
+
+describe('DKIM', function () {
+
+  describe('processSignature()', function () {
+    context('When Gmail raw message has multiple signatures', function () {
+
+      let processSignature = DKIM.verify.processSignature
+
+      it('excludes extra signature headers', function () {
+        let headers = require('./data/verify-headers')
+        let signatureHeader = require('./data/verify-signature-header')
+        let otherSignatureHeader = require('./data/verify-other-signature-header')
+
+        let result = processSignature(headers, signatureHeader)
+
+        assert.ok(headers.includes(signatureHeader))
+        assert.ok(headers.includes(otherSignatureHeader))
+        assert.ok(result.includes(signatureHeader))
+        assert.ok(!result.includes(otherSignatureHeader))
+      })
+    })
+
+  })
+
+
+  describe('Acceptence test - verify()', function () {
+
+    context('When provided Gmail raw message', function () {
+
+      it('returns an object with verified set to true', function (done) {
+
+        // @NOTE we add one line at the end of the raw message
+        raw = raw + '\n'
+        let message = new Buffer(raw.replace(/\n/g, '\r\n'))
+
+        DKIM.verify(message, function (e, res) {
+          assert.ok(res.every(record => record.verified))
+          done()
+        })
+      })
+
+    })
+  })
+})
