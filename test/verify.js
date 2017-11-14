afc233f26439632925b1974e96d7b40f8e7f96c8
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-11-14 12:38:02 +0100

    fix(verify): Fix stall when no signatures are found
    Closes #4

--- test/verify.js
+++ test/verify.js
@@ -1,13 +1,55 @@
-var assert = require('assert')
-var raw = require('./data/raw-message')
-var DKIM = require('..')
+var assert = require( 'assert' )
+var fs = require( 'fs' )
+var path = require( 'path' )
+var DKIM = require( '..' )
 
 describe('DKIM', function () {
 
+  describe('.verify()', function () {
+
+    context('When provided Gmail raw message', function () {
+
+      it('returns an object with verified set to true', function (done) {
+
+        var message = fs.readFileSync( path.join( __dirname, 'data', 'gmail-raw.txt' ) )
+
+        DKIM.verify( message, function( error, res ) {
+          assert.ifError( error )
+          assert.ok( res && res.length > 0 )
+          assert.ok( res.every( function( record ) {
+            return record.verified
+          }))
+          done( error )
+        })
+
+      })
+
+    })
+
+    context( 'email contains no signatures', function() {
+
+      it( 'does not hang', function( done ) {
+
+        var message = fs.readFileSync( path.join( __dirname, 'data', 'no-signatures.txt' ) )
+
+        DKIM.verify( message, function( error, res ) {
+          assert.ifError( error )
+          assert.equal( res.length, 0 )
+          done( error )
+        })
+
+      })
+
+    })
+
+  })
+
   describe('.verify.filterSignatureHeaders()', function () {
+
     context('When Gmail raw message has multiple signatures', function () {
 
       it('excludes extra signature headers', function () {
+
         var headers = require('./data/verify-headers')
         var signatureHeader = require('./data/verify-signature-header')
         var otherSignatureHeader = require('./data/verify-other-signature-header')
@@ -18,27 +60,11 @@ describe('DKIM', function () {
         assert.ok(headers.indexOf(otherSignatureHeader) > -1)
         assert.ok(result.indexOf(signatureHeader) > -1)
         assert.ok(!result.indexOf(otherSignatureHeader) > -1)
+
       })
+
     })
 
   })
 
-
-  describe('Acceptence test - verify()', function () {
-
-    context('When provided Gmail raw message', function () {
-
-      it('returns an object with verified set to true', function (done) {
-
-        // "raw" is a string, so we have to convert new lines to "text-file's new lines"
-        var message = new Buffer(raw.replace(/\n/g, '\r\n'))
-
-        DKIM.verify(message, function (e, res) {
-          assert.ok(res.every(function(record) { return record.verified; }))
-          done()
-        })
-      })
-
-    })
-  })
 })
